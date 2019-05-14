cbf56c45f58d21ac230e49f7d683934904a07af0
flexkat <catherinemaritz1@gmail.com>
2019-05-14 16:30:56 +0100

    star wars movie titles based on character

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -16,39 +16,42 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-  
-  film_array = response_hash["results"][0]["films"]
-  
   film_array_of_hashes = []
-  film_array.each do |film|
-    film.to_s
-    film_info = RestClient.get(film)
-    film_array_of_hashes << JSON.parse(film_info)
+  response_hash["results"].each do |characters|
+    characters["name"]
+    if characters["name"] == character_name
+      film_array = characters["films"]
+      
+      film_array.each do |film|
+        film.to_s
+        film_info = RestClient.get(film)
+        film_array_of_hashes << JSON.parse(film_info)
+      end
+    end
   end
   film_array_of_hashes
 end
+
+# get_character_movies_from_api("R5-D4")
 
 
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
   i = 0
- while i < films.length
+ while i < films.count
   puts films[i]["title"] 
   i += 1
  end
-  
 end
-
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
 
   print_movies(films)
-
 end
 
-show_character_movies("luke skywalker")
+show_character_movies("Darth Vader")
 
 ## BONUS
 
